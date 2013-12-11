6310641cf4ca2a641e1c0f7bf255802123e7d857
kaufmanno <olivier.kaufmann@umons.ac.be>
2013-12-11 23:35:47 +0100

    update

--- simpleDASttytunnel.py
+++ simpleDASttytunnel.py
@@ -43,5 +43,5 @@ while 1:
 #        print(repr(data))
         ConnectedClient.send(data)  # send data to client
 ConnectedClient.close()
-LocalSerialDas.connection.close()
+#LocalSerialDas.connection.close()
 print('Received', repr(data))

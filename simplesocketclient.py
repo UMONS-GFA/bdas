6fdd250266c4fc8463746c0f72d11e84c559d999
kaufmanno <olivier.kaufmann@umons.ac.be>
2013-12-12 00:24:00 +0100

    clean code

--- simplesocketclient.py
+++ simplesocketclient.py
@@ -1,24 +1,34 @@
-__author__ = 'Olivier Kaufmann'
 # Warning : Don't forget to start the server before running this script
+#-*- coding: utf8 -*-
+
+__author__ = 'Olivier Kaufmann'
+
 
 import sys
 import socket
+from settings import LocalHost, LocalPort
 
-Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-Host = 'mg3d.umons.ac.be'  # Insert remote server ip or name here
-Port = 10001  # Insert TCP port open for communications here
+#Host = '10.107.10.41'  # Insert remote server ip or name here
+#Port = 10001  # Insert TCP port open for communications here
+Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-Sock.connect((Host, Port))
+print('trying to connect...')
 
-print 'Socket connected'
+try:
+    Sock.connect((LocalHost, LocalPort))
+except socket.error as err:
+    print('connection failed : %s ' % err)
+    sys.exit()
+
+print('Socket connected')
 
 if len(sys.argv) == 2:
         cmd = str(sys.argv[1])
 else:
-        cmd = '-005'
+        cmd = '#E1'
 cmd += '\n\r'
-print 'Command :', repr(cmd)
-Sock.send(cmd)
-data = Sock.recv(1024)
+print('Command :', cmd.encode('utf-8'))
+Sock.send(bytearray(cmd,'ascii'))
+data = str(Sock.recv(255))
 Sock.close()
-print 'Received', repr(data)
+print('Received', str(data.encode('utf-8')))

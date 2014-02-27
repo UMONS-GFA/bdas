d2f7b2d3958c47bdebd0566eb974782af3b7046b
kaufmanno <olivier.kaufmann@umons.ac.be>
2014-02-28 00:10:21 +0100

    updated client - use with simpleDAStunnel.py to reach a DAS on RemoteHost or simpleDASttytunnel.py to reach a DAS on a serial port on LocalHost

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ from settings import LocalHost, LocalPort
 Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 data = bytearray()
-timeout = 3
+timeout = 0.2
 
 print('trying to connect...')
 
@@ -25,21 +25,19 @@ except socket.error as err:
 print('Socket connected')
 
 if len(sys.argv) == 2:
-        cmd = str(sys.argv[1])
+    cmd = str(sys.argv[1])
 else:
-        cmd = '#E0'
-cmd += '\n\r'
+    cmd = input('Type command (type #HE for help or exit to quit).\n> ')
+cmd += '\r'
 cmd = bytearray(cmd.encode('ascii'))
 Sock.setblocking(0)
 newline = False
 while 1:
-    print('>', cmd.decode())
     Sock.send(cmd)
     #beginning time
     starttime = time.time()
     while 1:
         #if you got some data, then break after timeout
-        # TODO :  (fix me) doesn't seem to break if DAS period is 1s
         if data and time.time()-starttime > timeout:
             break
 
@@ -67,8 +65,8 @@ while 1:
         except:
             pass
     data = bytearray()
-    cmd = input('Type command (type #HE for help or exit to quit).\n> ')
-    if not cmd or cmd.lower() == 'exit':
+    cmd = sys.stdin.readline()
+    if cmd.lower() == 'exit\n':
         break
     else:
         cmd += '\r'

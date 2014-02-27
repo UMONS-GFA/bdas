47a4917645fde0eab6c018ee69d5add221c262da
Christophe Bastin <christophe.bastin@umons.ac.be>
2014-02-27 12:18:00 +0100

    add  functions and variables

--- edas/edas-2001.c
+++ edas/edas-2001.c
@@ -59,3 +59,100 @@ const DWORD SEC_MOIS[12]=
     26265600uL,     /* + 31x24x3600   31 OCT */
     28857600uL      /* + 31x24x3600   30 NOV */
     };
+
+/* enums */
+enum {PIOA = 0x1c};
+enum {PIOB = 0x1e};
+enum {DART1 = -2, DART2 = -3};
+/* commands codes in hexadecimal */
+enum Ordres {E0 = 0x3045, E1 = 0x3145, E2 = 0x3245,
+             RI = 0x4952, SD = 0x4453, XB = 0x4258,
+             XP = 0x5058, XN = 0x4E58, ZR = 0x525A,
+             ZF = 0x465A, HE = 0x4548, WB = 0x4257,
+             XS = 0x5358, RM = 0x4D52, EW = 0x5745,
+             RW = 0x5752, SR = 0x5253, RL = 0x4C52,
+             RV = 0x5652, SI = 0x4953, RL = 0x4C52,
+             SH = 0x4853};
+
+/* Assembler Fonctions */
+extern int wtsdart1(void);
+extern void rd_s232(void);
+extern void watch_dog(void);
+extern void disable_dog(void);
+extern void sr_init(void);
+extern void sr_read(void);
+extern void sr_write(char *, char, char);
+extern void testcts(void);
+
+/* C functions */
+void RSHandle(char);
+void NMIHandle(void);
+void DoItNow(WORD);
+void Flash_Erase(void);
+void Flash_Write(BYTE, WORD, BYTE);
+void Flash_Sector_Erase(BYTE);
+DWORD Z80toUnix(int, int, int, int, int, int);
+void Read_Config(void);
+void Write_Config(void);
+BYTE Flash_READ(BYTE, WORD);
+BYTE SecFlash_Read(BYTE, WORD);
+void Centrage(void);
+void Download(DWORD, DWORD);
+void Write_Workbook_Event(BYTE);
+void DWORD_in_ParamStr(int, DWORD);
+void WORD _in_ParamStr(int, WORD);
+void Default_Config(void);
+void Data_Lost(void);
+void Clear_s232(void);
+void Sec_Flash_Write(BYTE, WORD, BYTE);
+void delay_mus(BYTE);
+
+/* Constants */
+const BYTE HEADER_EPROM[] = {"NANODAS ROB-ASO"};
+
+/* Variables [RS-232] */
+BYTE combuff_a[MAX_BUFF].combuff_b[MAX_BUFF];
+BYTE *s232;
+
+/* Variables [NMI] */
+BYTE NMI_i, NMI_j;
+WORD NMD_Global_Addy;
+WORD NMI_i_w;
+
+/* Variables [Need'em] */
+BYTE i,j;
+BYTE Erase_Flag, DowLoad_Flag, Centrage_Flag, Flag,
+                DLInterrupt_Flag, WriteData_Flag;
+
+BYTE DLStart_Flag;
+BYTE NextSector_Flag, RWrite_Flag, NMIoccur_Flag;
+
+/* Variables [System] */
+char flpioa, flpiob, cptpup, ctdog, flpout, addis ;
+BYTE R_Echo, ValidO;
+
+/* Variables [RSHandle] */
+BYTE Order; /* Received RS232 Command */
+
+/* Variables [Acquisition] */
+DWORD Chan[4], ChanTmp; /* Channel Values */
+BYTE LS590[20]; /* Individual counter values */
+DWORD Now_Unix, Log_Unix;
+BYTE Now_Byte1, Now_Byte2, Now_Byte3, Now_Byte4;
+BYTE LMin, LSec;
+
+/* Variables [Temp & Idle] */
+BYTE NMI_Tempo;
+BYTE logi_sect;
+WORD logi_addy;
+WORD Laps;
+WORD mode_eveil;
+BYTE Useldle_Flag, YetConnect_Flag;
+BYTE *ctsval;
+
+/* Variable [Workbook] */
+char ParamStr[75];
+BYTE ParamSize = 0;
+
+
+

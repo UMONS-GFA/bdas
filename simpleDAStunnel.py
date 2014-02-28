53a744b85a6f5c64131cc4f5e0399e194ea26a72
kaufmanno <olivier.kaufmann@umons.ac.be>
2014-02-28 15:13:25 +0100

    Add EOL Handle empty command

--- simpleDAStunnel.py
+++ simpleDAStunnel.py
@@ -1,7 +1,8 @@
 __author__ = 'Olivier Kaufmann'
 
-from settings import LocalHost, LocalPort, RemoteHost, RemotePort
+from settings import LocalHost, LocalPort, RemoteHost, RemotePort, EOL
 import socket
+
 
 ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket for communication with connecting clients
 ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # for socket reuse if interrupted (avoid [Errno 98] Address already in use) TODO : check whether this works...
@@ -19,11 +20,14 @@ while 1:
         cmd = ConnectedClient.recv(4)  # receive command from client
         if not cmd :
                 break
-        if cmd.decode('ascii') != '\n\r':
+        check=cmd.decode('ascii').replace('\r','')
+        if check != '':
             print('Received command', repr(cmd), 'from', address)
             ClientSocket.send(cmd)  # send command to remote host
             print('Sent command', cmd, 'to', RemoteHost, ':', RemotePort)
         data = ClientSocket.recv(1024)  # receive data from remote host
+        while EOL not in data:
+            data += ClientSocket.recv(1024)  # receive data from remote host
         print('Received data from', RemoteHost, ':', RemotePort)
         print(data)
         ConnectedClient.send(data)  # send data to client

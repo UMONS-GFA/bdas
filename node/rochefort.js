5c91821727f6c5647dec750e827f143a6067c183
Christophe Bastin <christophe.bastin@umons.ac.be>
2014-03-03 12:23:32 +0100

    fix coding style

--- node/rochefort.js
+++ node/rochefort.js
@@ -7,30 +7,21 @@ node rochefort.js
 Requirements(on the server):
 - Node.js
 - socket.io
-
-Access from the client:
-10.107.10.41:8000
-
-A Settings file must be created !!!
 */
 
-var http = require('http');
-var fs = require('fs');
-var net = require('net');
+var http = require('http');                     // used for webserver
+var fs = require('fs');                         // used to read file
+var net = require('net');                       // used to access serialport
 var settings = require('./settings').settings;
 
-console.log(settings.remoteHost);
-
 var client = net.connect({port: settings.remotePort, host: settings.remoteHost},
-    function() { //'connect' listener
+    function () {
         console.log('client connected');
     });
-
 client.setEncoding('utf-8');
 
-
-var webserver = http.createServer(function(request, response){
-    fs.readFile("client.html", 'utf-8', function(error, data){
+var webserver = http.createServer(function (request, response) {
+    fs.readFile("client.html", 'utf-8', function (error, data) {
         response.writeHead(200, {'Content-Type':'text/html'});
         response.write(data);
         response.end();
@@ -40,32 +31,28 @@ var webserver = http.createServer(function(request, response){
 var io = require('socket.io').listen(webserver);
 
 var readData = '';
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function (socket) {
     console.log("Socket connected");
-    socket.on('messaqe_to_server', function(command){
-        // show command sent by the client
-        client.write(command+'\r');
-        if(command == 'exit') {
+    socket.on('messaqe_to_server', function (command) {
+        client.write(command + settings.EOL);
+        if (command == 'exit') {
             process.exit(code=0);
         }
         console.log(command);
     });
 
-    client.on('data', function(data) {
+    client.on('data', function (data) {
         readData += data.toString();
-        console.log("Readdata :"+ readData);
-        if(readData.indexOf('*') >= 0 && readData.indexOf('\r') >= 0){
+        console.log("Readdata :" + readData);
+        if (readData.indexOf('*') >= 0 && readData.indexOf('\r') >= 0) {
             sendData = readData .substring(readData .indexOf('*'), readData.indexOf('\r'));
             io.sockets.emit("message_to_client",{ response: sendData});
             console.log(sendData);
             readData = '';
         }
-
-
-        //client.end();
     });
 
-    client.on('end', function() {
+    client.on('end', function () {
         console.log('client disconnected');
     });
 });

b131fbd03d87b7da3e2d5573ec9b042fadfea31e
bastinc <christophe.bastin@umons.ac.be>
2014-05-21 14:49:45 +0200

    show log if tunnel is opened

--- DAStunnel.py
+++ DAStunnel.py
@@ -2,13 +2,17 @@ __author__ = 'Olivier Kaufmann'
 
 from settings import LocalPort, RemoteHost, RemotePort
 import sys
-import subprocess
+import subprocess   # used to launch shell command
+
 
 print('Opening tunnel...')
 try:
+    # launch netcat for proxy server socket
     retcode = subprocess.call('nc -l -p %i -c "nc %s %i"' % (LocalPort, RemoteHost, RemotePort), shell=True)
     if retcode < 0:
         print('Process was terminated by signal', -retcode, file=sys.stderr)
+    elif retcode == 0:
+        print("Tunnel opened")
     else:
         print('Process returned', retcode, file=sys.stderr)
 except OSError as e:

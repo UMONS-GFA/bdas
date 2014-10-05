daaa84e67bdc52fb0d39d0d70daf27c92ee0a9f1
kaufmanno <olivier.kaufmann@umons.ac.be>
2014-10-05 20:33:10 +0200

    testGeoJSON

--- draft/station.py
+++ draft/station.py
@@ -1,11 +1,28 @@
 __author__ = 'su530201'
 from osgeo import ogr # requires ogr
 
-DEBUG = True
+# DEBUG = True
+#
+# class Station(object):
+#     name = ''
+#     point = ogr.Geometry(ogr.wkbPoint)
+#     point.AddPoint(1198054.34, 648493.09)
+#
+#
+#     def __init__(self, a_name='unknown'):
+#         self.name = a_name
 
-class Station(object):
-    name = ''
-    point = ogr.Geometry(ogr.wkbPoint)
+from osgeo import ogr # requires ogr
 
-    def __init__(self, a_name='unknown'):
-        self.name = a_name
+LB72 = ogr.osr.SpatialReference()
+LB72.ImportFromEPSG(31370)
+WGS84 = ogr.osr.SpatialReference()
+WGS84.ImportFromEPSG(4326)
+
+point = ogr.Geometry(ogr.wkbPoint)
+point.AddPoint(119805.00, 84930.00)
+point.AssignSpatialReference(LB72)
+point.TransformTo(WGS84)
+
+geojson_point = point.ExportToJson()
+print(geojson_point)

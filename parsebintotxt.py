5af80263b0b7b6326ecf32ad5df0645cfad176bb
kaufmanno <olivier.kaufmann@umons.ac.be>
2014-10-22 19:43:59 +0200

    use streams

--- parsebintotxt.py
+++ parsebintotxt.py
@@ -1,252 +1,54 @@
 """
-A binary to txt parser
-This program let you choose a binary file and propose to parse and save it as a readable text file.
+A DAS binary file to text file parser
+This program let you choose one or more binary file(s) and propose to parse and save it (them) as readable text file.
 
  """
 import os
-import datetime
+import parsedasbin
 from tkinter import filedialog
 
-# if ask_files is true, asks for input and output files
-ask_files = False
-in_filename = '/home/su530201/PycharmProjects/DownloadDAS/R013Full_20141010_0600.bin'
-out_filename = '/home/su530201/PycharmProjects/DownloadDAS/R013Full_20141010_0600'
 
-# time step between two data [s]
-time_step = 1
-
-# if double_ff_flag is true, reads 4 ff after the fd block instead of 2 ff...
-double_ff_flag = True
-
-# if check_length is true, the process will end after kmax
-check_length_flag = True
-k = 1
-k_max = 350000
-
-# if date_as_secs_since_epoch is true, converts to epoch otherwise to string date
-date_as_secs_since_epoch = False
-
-# verbose_flag
-verbose_flag = False
-
-# separator in text file
-sep = ','
-
-#outfile extension
-ext = '.txt'
-
-# dtm format for mgr compatibility
-dtm_format = True
-site = '0000'
-netid = '255'
-data = ''
-bpos = '1'
-
-if dtm_format:
-    date_as_secs_since_epoch = False
-    sep = ' '
-    ext = '.dtm'
-
-out_filename += ext
-
-if ask_files:
-    home = os.path.expanduser('~')
-    in_filename = filedialog.askopenfilename(filetypes=('binary {*.bin}', 'Binary files'),
-                                             initialdir = home, initialfile = '')
-    out_filename = os.path.splitext(os.path.basename(in_filename))[0]
-    outfile = filedialog.asksaveasfile(filetypes=("Text files", "text {*.txt}"), initialfile = out_filename + ext)
-else:
-    outfile = open(out_filename, 'wt')
-
-infile = open(in_filename, 'rb')
-
-# reading begin message
-b = infile.read(1)
-i = 0
-while b == b'\xfd':
-    i += 1
-    b = infile.read(1)
-    if verbose_flag:
-        print(b)
-
-if (i % 3 == 0) & (i > 0):
-    n_channels = int(i / 3)
-    if verbose_flag:
-        print('Number of channels :' + str(n_channels))
-    eot = False
-else:
-    print('Format error : unexpected number of leading 0xFD!:' + str(i))
-    n_channels = 0
-    eot = True
-    exit()
-
-b1 = infile.read(1)
-
-# looking for extra 00
-if b == b'\x00':
-    b = b1
-    b1 = infile.read(1)
-    if verbose_flag:
-        print('Warning : extra 00 after leading 0xFD!')
-
-if dtm_format:
-    ## write header
-    s = '# SITE: ' + site
-    outfile.writelines(s + '\n')
-    s = '# UDAS: ' + netid
-    outfile.writelines(s + '\n')
-    s = '# CHAN: YYYY MO DD HH MI SS'
-    for i in range(n_channels):
-        s = s + ' ' + format(i + 1, '04d')
-    outfile.writelines(s + '\n')
-    s = '# DATA: ' + data
-    outfile.writelines(s + '\n')
-    s = '# BPOS: ' + bpos
-    outfile.writelines(s + '\n')
-
-# reading date block FF FF
-if (b == b'\xff') and (b1 == b'\xff'):
-    date_block = True
-else:
-    print('Format error : Date block not detected...')
-    if verbose_flag:
-        print('Unexpected values for the 2 bytes following 0xFD!)')
-        print(b)
-        print(b1)
-    date_block = False
-    exit()
-
-# reading D1 D2 D3 D4
-b = infile.read(4)
-while date_block:
-    while b[0].to_bytes(1, 'big') == b'\xff':
-        if b[1].to_bytes(1, 'big') == b'\xff':
-            double_ff_flag = True
-            if verbose_flag:
-                print('Doubled 0xFF 0xFF flag activated.')
-            b1 = infile.read(2)
-            b = b[2:4] + b1
+def parse_bin_files_to_text_files(in_filename='', out_filename='', time_step=60, k_max=330000,
+                                   date_as_secs_since_epoch=False, verbose_flag=False, sep=',', ext='.txt', site='0000',
+                                   netid='255', data='', bpos='1', date_format='%Y/%m/%d %H:%M:%S'):
+    dtm_format = False
+    if in_filename == '':
+        # if ask_files is true, asks for input and output files
+        ask_files = True
+    else:
+        ask_files = False
+        if out_filename == '':
+            out_filename = os.path.splitext(os.path.abspath(in_filename))[0]
         else:
-            print('Error : unexpected format, 3 consecutive 0xFF in date block !')
-            exit()
-        secs_since_epoch = int.from_bytes(b, byteorder='big')
-        cur_time = datetime.datetime.utcfromtimestamp(secs_since_epoch)
-        if verbose_flag:
-            print('starting date:' + cur_time.strftime('%d/%m/%Y %H:%M:%S'))
-        if dtm_format:
-            s = '# INFO: interruption ' + cur_time.strftime('%Y %m %d %H %M %S') + '\n'
-            outfile.writelines(s)
-        date_block = True
+            out_filename = os.path.splitext(os.path.abspath(out_filename))[0]  # remove extension if stated
 
-        # reading optional 00 00 00
-        for i in range(3, n_channels + 1):
-            b = infile.read(3)
-            if b != b'\x00\x00\x00':
-                print('format error : unexpected values for the bytes following the date section!')
-                print(b)
-        b = infile.read(2)
-        if b == b'\xff\xff':
-            date_block = True
-            b = infile.read(4)
+    if ext == '.dtm':
+        date_as_secs_since_epoch = False
+        sep = ' '
+        dtm_format = True
+
+    out_filename += ext
+
+    if ask_files:
+        home = os.path.expanduser('~')
+        in_filename = filedialog.askopenfilename(filetypes=('binary {*.bin}', 'Binary files'),
+                                                 initialdir = home, initialfile = '')
+        out_filename = os.path.splitext(os.path.basename(in_filename))[0]
+        if dtm_format:
+            outfile = filedialog.asksaveasfile(filetypes=("Text files", "text {*.dtm}"),
+                                               initialfile = out_filename + ext)
         else:
-            date_block = False
+            outfile = filedialog.asksaveasfile(filetypes=("Text files", "text {*." + ext + "}"),
+                                               initialfile = out_filename + ext)
+    else:
+        outfile = open(out_filename, 'wt')
 
-# reading channels and writing dat file
-if not eot:
-    #outfile = open(out_filename, 'wt+')
-    try:
-        ## write data
-        infile.seek(-2, 1)
-        while not eot:
-            channel = []
-            eot = True
-            logged_event = False
-            for j in range(n_channels):
-                sb = infile.read(3)
-                if sb[0] == 0xff:
-                    if double_ff_flag:
-                        b = infile.read(1)
-                        sb = sb[0:1] + sb[2:3] + b
-                        if verbose_flag:
-                            print('Info : removing repeated ff')
-                if sb[1] == 0xff:
-                    if double_ff_flag:
-                        b = infile.read(1)
-                        sb = sb[0:1] + sb[2:3] + b
-                        if verbose_flag:
-                            print('Info : removing repeated ff')
-                if sb[2] == 0xff:
-                    if double_ff_flag:
-                        b = infile.read(1)
-                        sb = sb[0:2] + b  # TODO : Check this...
-                        if verbose_flag:
-                            print('Info : removing repeated ff')
+    k_tot = round((os.stat(in_filename).st_size - 40)/12, 0)
+    infile = open(in_filename,'rb')
+    parsedasbin.parse_bin_to_ascii(infile, outfile, time_step, 0, k_tot, date_as_secs_since_epoch,
+                                   verbose_flag, sep, ext, site, netid, data, bpos, date_format)
+    infile.close()
+    outfile.close()
 
-                # logged event ?
-                if (sb[0] == 0xff) and (sb[1] == 0xff):
-                    logged_event = True
-                    l = 0
-                    infile.seek(-1, 1)
-                    sb = b''
-                    while l < 10:
-                        b = infile.read(1)
-                        if (b == b'\xff') and double_ff_flag:
-                            b = infile.read(1)
-                        sb += b
-                        l += 1
-                    event_time = datetime.datetime.utcfromtimestamp(int.from_bytes(sb[0:4], 'big'))
-                    if verbose_flag:
-                        print(event_time.strftime('%Y/%m/%d %H:%M:%S'), 'Event recorded')
-                    if dtm_format:
-                        s = '# INFO: interruption ' + event_time.strftime('%Y %m %d %H %M %S') + '\n'
-                        outfile.writelines(s)
-                    else:
-                        if date_as_secs_since_epoch:
-                            s = 'not implemented yet'  # TODO : implement this condition...
-                        else:
-                            s = event_time.strftime('%Y/%m/%d %H:%M:%S')
-                        s += ' : Event recorded'
-                        outfile.writelines(s + '\n')
-                    eot = False
-                    break
-                else:
-                    channel.append(int.from_bytes(sb, byteorder='big'))
-                    if channel[j] != 0xfefefe:
-                        eot = False
-
-            if not logged_event:
-                secs_since_epoch += time_step    # TODO : check workbook for stops and set date.
-                cur_time = datetime.datetime.utcfromtimestamp(secs_since_epoch)
-
-                if verbose_flag:
-                    print(cur_time.strftime('%Y/%m/%d %H:%M:%S'), map(hex, channel[0:n_channels]))
-                if date_as_secs_since_epoch:
-                    s = str(secs_since_epoch)
-                else:
-                    if dtm_format:
-                        s = cur_time.strftime('%Y %m %d %H %M %S')
-                    else:
-                        s = cur_time.strftime('%Y/%m/%d %H:%M:%S')
-                if not eot:
-                    for j in range(n_channels):
-                        if dtm_format:
-                            t = format(channel[j], '013.4f')
-                            u = 13
-                        else:
-                            t = format(channel[j], '05d')
-                            u = 5
-                        s += sep + t[len(t) - u:len(t)]
-                    outfile.writelines(s + '\n')
-                if k > k_max:
-                    if verbose_flag:
-                        print('Too much rows...')
-                    eot = True
-                k += 1
-        if dtm_format:
-            s = '# INFO: End Of File\n'
-            outfile.writelines(s)
-
-    finally:
-        infile.close()
-        outfile.close()
-        print('Ending bin parsing.')
+if __name__ == '__main__':
+    parse_bin_files_to_text_files()
--- parsedasbin.py
+++ parsedasbin.py
@@ -3,13 +3,11 @@ A binary to txt parser
 This program let you choose a binary file and propose to parse and save it as a readable text file.
 
  """
-import os
 import datetime
 import logging
-from tkinter import filedialog
 
 
-def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=330000, date_as_secs_since_epoch=False,
+def parse_bin_to_ascii(in_stream, out_stream, time_step=60, k_max=330000, k_tot=0, date_as_secs_since_epoch=False,
                        verbose_flag=False, sep=',', ext='.txt', site='0000', netid='255', data='', bpos='1',
                        date_format='%Y/%m/%d %H:%M:%S'):
     # in_filename : bin file to convert
@@ -17,7 +15,7 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
     # if date_as_secs_since_epoch is true, converts to epoch otherwise to string date
     # if verbose_flag is true, prints messages while processing
     # sep is the separator in the output text file
-    # ext is the outfile extension (choose .dtm for compatibility with edas44 format for mgr)
+    # ext is the out_stream extension (choose .dtm for compatibility with edas44 format for mgr)
 
     status = 0
     dtm_format = False
@@ -35,63 +33,23 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
     # 1024 : Unexpected values for the bytes following the date in date block
     # 2048 : Date block missing in sector [0]
 
-    if in_filename == '':
-        # if ask_files is true, asks for input and output files
-        ask_files = True
-    else:
-        ask_files = False
-        if out_filename == '':
-            out_filename = os.path.splitext(os.path.abspath(in_filename))[0]
-        else:
-            out_filename = os.path.splitext(os.path.abspath(out_filename))[0]  # remove extension if stated
-
-    if ext == '.dtm':
-        date_as_secs_since_epoch = False
-        sep = ' '
-        dtm_format = True
-
-    out_filename += ext
-
-    if ask_files:
-        home = os.path.expanduser('~')
-        in_filename = filedialog.askopenfilename(filetypes=('binary {*.bin}', 'Binary files'),
-                                                 initialdir = home, initialfile = '')
-        out_filename = os.path.splitext(os.path.basename(in_filename))[0]
-        if dtm_format:
-            outfile = filedialog.asksaveasfile(filetypes=("Text files", "text {*.dtm}"),
-                                               initialfile = out_filename + ext)
-        else:
-            outfile = filedialog.asksaveasfile(filetypes=("Text files", "text {*." + ext + "}"),
-                                               initialfile = out_filename + ext)
-    else:
-        outfile = open(out_filename, 'wt')
-
     # if double_ff_flag is true, reads two successive ff as one single ff...
     double_ff_flag = False
 
     if k_max > 0:
         # if check_length is true, the process will end after kmax
         check_length_flag = True
+    elif k_tot > 0:
+        k_max = k_tot
+        check_length_flag = True
     k = 1
 
-    if verbose_flag:
-        s = 'Parsing DAS bin file... \nin_filename : ' + in_filename + ' \nout_filename : ' + out_filename \
-            + '\nstation : ' + site + ' DAS number : ' + netid + ' integration period : ' + str(time_step)
-        if date_as_secs_since_epoch:
-            s += ' s \ndate as seconds since epoch'
-        else:
-            s += 's \ndate as string'
-        logging.info(s)
-
-    k_tot = round((os.stat(in_filename).st_size - 40)/12, 0)
-    infile = open(in_filename, 'rb')
-
     # reading begin message
-    b = infile.read(1)
+    b = in_stream.read(1)
     i = 0
     while b == b'\xfd':
         i += 1
-        b = infile.read(1)
+        b = in_stream.read(1)
 
     if (i % 3 == 0) & (i > 0):
         n_channels = int(i/3)
@@ -107,33 +65,33 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
 
     # looking for extra 00
     if b == b'\x00':
-        infile.seek(1, 1)
+        in_stream.seek(1, 1)
         logging.warning('*** Extra 0x00 after leading 0xFD!')
         status += 2
 
     if dtm_format:
-        ## write header
+        ## writing header
         s = '# SITE: ' + site
-        outfile.writelines(s + '\n')
+        out_stream.writelines(s + '\n')
         s = '# UDAS: ' + netid
-        outfile.writelines(s + '\n')
+        out_stream.writelines(s + '\n')
         s = '# CHAN: YYYY MO DD HH MI SS'
         for i in range(n_channels):
             s = s + ' ' + format(i + 1, '04d')
-        outfile.writelines(s + '\n')
+        out_stream.writelines(s + '\n')
         s = '# DATA: ' + data
-        outfile.writelines(s + '\n')
+        out_stream.writelines(s + '\n')
         s = '# BPOS: ' + bpos
-        outfile.writelines(s + '\n')
+        out_stream.writelines(s + '\n')
 
     # reading date block FF FF
-    infile.seek(-1, 1)
-    b = infile.read(2)
+    in_stream.seek(-1, 1)
+    b = in_stream.read(2)
     if b == b'\xff\xff':
         date_block = True
     elif b == b'\x70\x61':
-        infile.seek(-2, 1)
-        b = infile.read(47)
+        in_stream.seek(-2, 1)
+        b = in_stream.read(47)
         if b == b'pas de bloc date en debut de secteur numero [0]':
             logging.error('*** Date block missing in sector [0]!')
             status += 2048
@@ -150,7 +108,7 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
         return status
 
     # reading D1 D2 D3 D4
-    b = infile.read(4)
+    b = in_stream.read(4)
     while date_block:
         while b[0].to_bytes(1, 'big') == b'\xff':
             if b[1].to_bytes(1, 'big') == b'\xff':
@@ -158,7 +116,7 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
                     double_ff_flag = True
                     logging.warning('*** Doubled 0xFF flag activated.')
                     status += 8
-                b = b[2:4] + infile.read(2)
+                b = b[2:4] + in_stream.read(2)
 
             else:
                 logging.error('*** Unexpected format, 3 consecutive 0xFF in date block !')
@@ -170,57 +128,55 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
             logging.info('Starting date:' + cur_time.strftime('%d/%m/%Y %H:%M:%S'))
         if dtm_format:
             s = '# INFO: interruption ' + cur_time.strftime('%Y %m %d %H %M %S') + '\n'
-            outfile.writelines(s)
+            out_stream.writelines(s)
 
         # reading optional 00 00 00
         for i in range(3, n_channels + 1):
-            b = infile.read(3)
+            b = in_stream.read(3)
             if b != b'\x00\x00\x00':
                 logging.error('*** Unexpected values for the bytes following the date in date block!' + repr(b))
                 status += 1024
                 return status
-        b = infile.read(2)
+        b = in_stream.read(2)
         if b == b'\xff\xff':
             date_block = True
-            b = infile.read(4)
+            b = in_stream.read(4)
         else:
             date_block = False
 
-    # reading channels and writing dat file
+    # reading channels form in_stream and writing to out_stream
     if not eot:
-        #outfile = open(out_filename, 'wt+')
         try:
-            ## write data
-            infile.seek(-2, 1)
+            in_stream.seek(-2, 1)
             while not eot:
                 channel = []
                 eot = True
                 logged_event = False
                 for j in range(n_channels):
-                    sb = infile.read(3)
+                    sb = in_stream.read(3)
                     if len(sb) < 3:
                         if verbose_flag:
                             logging.warning('*** Unexpected end of file!')
                         if dtm_format:
                             s = '# INFO: End Of File\n'
-                            outfile.writelines(s)
+                            out_stream.writelines(s)
                         status += 4
                         return status
                     if sb[0] == 0xff:
                         if double_ff_flag:
-                            b = infile.read(1)
+                            b = in_stream.read(1)
                             sb = sb[0:1] + sb[2:3] + b
                             if verbose_flag:
                                 logging.info('Info : removing repeated 0xFF')
                     if sb[1] == 0xff:
                         if double_ff_flag:
-                            b = infile.read(1)
+                            b = in_stream.read(1)
                             sb = sb[0:1] + sb[2:3] + b
                             if verbose_flag:
                                 logging.info('Info : removing repeated 0xFF')
                     if sb[2] == 0xff:
                         if double_ff_flag:
-                            b = infile.read(1)
+                            b = in_stream.read(1)
                             sb = sb[0:2] + b  # TODO : Check this...
                             if verbose_flag:
                                 logging.info('Info : removing repeated 0xFF')
@@ -229,28 +185,28 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
                     if (sb[0] == 0xff) and (sb[1] == 0xff):
                         logged_event = True
                         l = 0
-                        infile.seek(-1, 1)
+                        in_stream.seek(-1, 1)
                         sb = b''
                         while l < 10:
-                            b = infile.read(1)
+                            b = in_stream.read(1)
                             if (b == b'\xff') and double_ff_flag:
-                                b = infile.read(1)
+                                b = in_stream.read(1)
                             sb += b
                             l += 1
                         event_time = datetime.datetime.utcfromtimestamp(int.from_bytes(sb[0:4], 'big'))
-                        cur_time = event_time
+                        secs_since_epoch = int.from_bytes(sb[0:4], 'big')
                         if verbose_flag:
                             logging.info(event_time.strftime('%Y/%m/%d %H:%M:%S') + ' Interruption')
                         if dtm_format:
                             s = '# INFO: interruption ' + event_time.strftime('%Y %m %d %H %M %S') + '\n'
-                            outfile.writelines(s)
+                            out_stream.writelines(s)
                         else:
                             if date_as_secs_since_epoch:
                                 s = 'not implemented yet'  # TODO : implement this condition...
                             else:
                                 s = event_time.strftime(date_format)
                             s += ' : Interruption'
-                            outfile.writelines(s + '\n')
+                            out_stream.writelines(s + '\n')
                         eot = False
                         break
                     else:
@@ -262,7 +218,7 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
                     secs_since_epoch += time_step    # TODO : check workbook for stops and set date.
                     cur_time = datetime.datetime.utcfromtimestamp(secs_since_epoch)
 
-                    if verbose_flag:
+                    if verbose_flag and k_tot>0:
                         if k/1024-round(k/1024) == 0:
                             print(repr(round(100*k/k_tot, 1)) + ' % done', end='\r')
                         # print(cur_time.strftime('%Y/%m/%d %H:%M:%S'), map(hex, channel[0:n_channels]))
@@ -282,7 +238,7 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
                                 t = format(channel[j], '05d')
                                 u = 5
                             s += sep + t[len(t) - u:len(t)]
-                        outfile.writelines(s + '\n')
+                        out_stream.writelines(s + '\n')
                     if (k > k_max) and check_length_flag:
                         if verbose_flag:
                             logging.warning('Too much rows...')
@@ -290,10 +246,8 @@ def parse_bin_to_ascii(in_filename='', out_filename='', time_step=60, k_max=3300
                     k += 1
             if dtm_format:
                 s = '# INFO: End Of File\n'
-                outfile.writelines(s)
+                out_stream.writelines(s)
 
         finally:
-            infile.close()
-            outfile.close()
             logging.info('Ending bin parsing.')
             return status

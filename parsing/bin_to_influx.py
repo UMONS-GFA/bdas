feba373e2b8f6117c2284869ced3b56f599f2499
bastinc <christophe.bastin@umons.ac.be>
2017-03-13 16:08:19 +0100

    refactor paths, fix #27

--- parsing/bin_to_influx.py
+++ parsing/bin_to_influx.py
@@ -1,23 +1,13 @@
 import glob
-import os
 import sys
 import logging
 import datetime
 import pandas as pd
+from os import path, makedirs, rename
 from influxdb import DataFrameClient
 from time import gmtime
 from parsing import bin2df
-from bdas.settings import influx_host, influx_port, influx_user, influx_password
-
-bin_path = '../DownloadDAS/'
-processed_dir = '/processed/'
-mask = 'R*'
-log_file = '../DownloadDAS/logs/bin_to_influx.log'
-influx_dbname = ''
-
-status = 0
-utc_tz = datetime.timezone.utc
-
+from bdas.settings import DATABASE, BIN_DIR, PROCESSED_DIR, LOG_DIR, LOG_FILE, MASK
 
 
 def bin_to_influx(bin_filename, last_date):
@@ -34,22 +24,25 @@ def bin_to_influx(bin_filename, last_date):
 
 if __name__ == "__main__":
     i = 1
-    if not os.path.exists(bin_path + '/logs/'):
-        os.makedirs(bin_path + '/logs/')
+    status = None
+    log_path = path.join(BIN_DIR, 'logs')
+    processed_path = path.join(BIN_DIR, 'processed')
+    if not path.exists(log_path):
+        makedirs(log_path)
     logging_level = logging.DEBUG
     logging.Formatter.converter = gmtime
     log_format = '%(asctime)-15s %(levelname)s:%(message)s'
     logging.basicConfig(format=log_format, datefmt='%Y/%m/%d %H:%M:%S UTC', level=logging_level,
-                    handlers=[logging.FileHandler(log_file), logging.StreamHandler()])
+                        handlers=[logging.FileHandler(path.join(LOG_DIR, LOG_FILE)), logging.StreamHandler()])
     logging.info('_____ Started _____')
 
     if len(sys.argv) > 1:
         if len(sys.argv) % 2 == 1:
             while i < len(sys.argv)-1:
-                if sys.argv[i] == 'mask':
-                    mask = str(sys.argv[i+1])
+                if sys.argv[i] == 'MASK':
+                    MASK = str(sys.argv[i+1])
                 elif sys.argv[i] == 'binpath':
-                    bin_path = str(sys.argv[i+1])
+                    BIN_DIR = str(sys.argv[i+1])
                 elif sys.argv[i] == 'dbname':
                     influx_dbname = str(sys.argv[i+1])
                 else:
@@ -64,17 +57,17 @@ if __name__ == "__main__":
     else:
         logging.warning('*** No argument found...')
 
-    bin_filenames = sorted(glob.iglob(bin_path+mask+'.bin'))
+    bin_filenames = sorted(glob.iglob(BIN_DIR+MASK+'.bin'))
     logging.info('%d bin files to process...' % len(bin_filenames))
 
     if len(bin_filenames) > 0:
-        client = DataFrameClient(influx_host, influx_port, influx_user, influx_password, influx_dbname)
-        if not os.path.exists(bin_path + processed_dir):
-            processed_path = bin_path + processed_dir
-            os.makedirs(processed_path)
+        client = DataFrameClient(DATABASE['HOST'], DATABASE['PORT'], DATABASE['USER'],
+                                 DATABASE['PASSWORD'], DATABASE['NAME'])
+        if not processed_path:
+            makedirs(processed_path)
         for f in bin_filenames:
             metadata = bin2df.get_metadata(f)
-            if not metadata is None:
+            if metadata is not None:
                 net_id = metadata['NetId']
                 last_measurement = client.query('select last(*) from "measurement" where "das"='+ net_id +';')
                 if not last_measurement:
@@ -83,7 +76,7 @@ if __name__ == "__main__":
                     ld = last_measurement['measurement'].index.to_pydatetime()[0]
                 status = bin_to_influx(f, ld)
                 if status == 0 or status == 1:
-                    os.rename(f, os.path.dirname(f) + processed_dir + os.path.basename(f))
+                    rename(f, path.dirname(f) + PROCESSED_DIR + path.basename(f))
                 else:
                     logging.warning('%s could not be processed...' % f)
     else:
